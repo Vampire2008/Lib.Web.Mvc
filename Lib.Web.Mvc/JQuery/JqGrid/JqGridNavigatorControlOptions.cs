6893eb580df57023019ab6329a211d155d943aec
Vampire_2008 <vampire_2007@mail.ru>
2018-06-08 11:29:20 +0800

    Added property that defines buttons or separators must be added after Inline Navigator buttons.

--- Lib.Web.Mvc/JQuery/JqGrid/JqGridNavigatorControlOptions.cs
+++ Lib.Web.Mvc/JQuery/JqGrid/JqGridNavigatorControlOptions.cs
@@ -1,25 +1,34 @@
 ﻿namespace Lib.Web.Mvc.JQuery.JqGrid
 {
-    /// <summary>
-    /// Base class for classes which represents options for jqGrid Navigator controls
-    /// </summary>
-    public abstract class JqGridNavigatorControlOptions
-    {
-        #region Properties
-        /// <summary>
-        /// Gets or sets the value which defines if the jqGrid Navigator control added to the bottom pager should be coppied to the top pager.
-        /// </summary>
-        public bool CloneToTop { get; set; }
-        #endregion
+	/// <summary>
+	/// Base class for classes which represents options for jqGrid Navigator controls
+	/// </summary>
+	public abstract class JqGridNavigatorControlOptions
+	{
+		#region Properties
+		/// <summary>
+		/// Gets or sets the value which defines if the jqGrid Navigator control added to the bottom pager should be coppied to the top pager.
+		/// </summary>
+		public bool CloneToTop { get; set; }
 
-        #region Constructor
-        /// <summary>
-        /// Initializes a new instance of the JqGridNavigatorControlOptions class.
-        /// </summary>
-        public JqGridNavigatorControlOptions()
-        {
-            CloneToTop = false;
-        }
-        #endregion
-    }
+		/// <summary>
+		/// Gets or sets the value which sets posistion of button
+		/// </summary>
+		public bool AddAfterInlineNavigator { get; set; }
+
+		internal bool ProcessedElement { get; set; }
+		#endregion
+
+		#region Constructor
+		/// <summary>
+		/// Initializes a new instance of the JqGridNavigatorControlOptions class.
+		/// </summary>
+		public JqGridNavigatorControlOptions()
+		{
+			CloneToTop = false;
+			AddAfterInlineNavigator = false;
+			ProcessedElement = false;
+		}
+		#endregion
+	}
 }
